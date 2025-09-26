7030539513e0c80f2d16d09718b2337a81a22080
Juan U <juan.lopez15@utp.edu.co>
2025-09-26 17:28:15 -0500

    fix: change pi estimation formula

--- PI/Needle/needle.c
+++ PI/Needle/needle.c
@@ -12,8 +12,6 @@
 #include <time.h>
 #include <math.h>
 
-#define NEEDLE_LENGTH 1.0 // Length of the needle
-#define LINE_DISTANCE 1.0 // Distance between parallel lines
 
 /**
  * Generates a random double between 0 and 1
@@ -26,55 +24,66 @@ double random_double()
 /**
  * Simulates dropping a single needle and checks if it crosses a line
  *
+ * @param line_distance distance between the parallel lines
+ * @param needle_length Length of the needle
  * @return 1 if needle crosses a line, 0 otherwise
  */
-int drop_needle()
+int drop_needle(double line_distance, double needle_length)
 {
     // Random position of the needle's center (distance from nearest line)
-    double position = random_double() * LINE_DISTANCE;
+    double position = random_double() * line_distance;
 
     // Random angle of the needle with respect to the lines (in radians)
     double angle = random_double() * M_PI;
 
     // Calculate the half-length projection on the x-axis
-    double half_length_projection = (NEEDLE_LENGTH / 2.0) * sin(angle);
+    double half_length_projection = (needle_length / 2.0) * sin(angle);
 
-    // Check if needle crosses a line (either at position=0 or position=LINE_DISTANCE)
-    return (position - half_length_projection < 0) || (position + half_length_projection > LINE_DISTANCE);
+    // Check if needle crosses a line (either at position=0 or position=line_distance)
+    return (position - half_length_projection < 0) || (position + half_length_projection > line_distance);
 }
 
 /**
  * Runs the Monte Carlo simulation with n needles
  *
+ * @param line_distance distance between the parallel lines
+ * @param needle_length Length of the needle
  * @param n Number of needles to drop
  * @return Estimated value of π
  */
-double estimate_pi(long n)
+double estimate_pi(double line_distance, double needle_length, long n)
 {
     long crossings = 0;
     for (long i = 0; i < n; i++)
     {
-        crossings += drop_needle();
+        crossings += drop_needle(line_distance, needle_length);
     }
 
     // For needles of length equal to line distance,
-    // π can be estimated as: 2 * (number of tosses) / (number of crossings)
-    return (2.0 * n) / crossings;
+    // General formula for any needle length (when needle_length ≤ line_distance)
+    return (2.0 * needle_length * n) / (crossings * line_distance);
 }
-
 
 int main(int argc, char *argv[])
 {
     // Check command line arguments
-    if (argc != 2) {
-        printf("Usage: %s <number_of_throws>\n", argv[0]);
+    if (argc != 4)
+    {
+        printf("Usage: %s <line_distance> <needle_length> <number_of_throws>\n", argv[0]);
         return 1;
     }
 
-    long n = atol(argv[1]);
-    
-    if (n <= 0) {
+    long line_distance = atol(argv[1]);
+    long needle_length = atol(argv[2]);
+    long n = atol(argv[3]);
+
+    if (n <= 0)
+    {
         printf("Error: Number of throws must be positive\n");
+        return 1;
+    }
+    if (needle_length > line_distance){
+        printf("Error: Needle length must not exceed line distance\n");
         return 1;
     }
 
@@ -84,7 +93,7 @@ int main(int argc, char *argv[])
     // Run simulation and measure time
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
-    double pi_estimate = estimate_pi(n);
+    double pi_estimate = estimate_pi(line_distance, needle_length, n);
     clock_gettime(CLOCK_MONOTONIC, &end);
 
     // Calculate execution time
@@ -99,4 +108,3 @@ int main(int argc, char *argv[])
 
     return EXIT_SUCCESS;
 }
-
