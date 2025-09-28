294556d01113b916db0a611fbe9c90dd7d69d6c4
Juan U <juan.lopez15@utp.edu.co>
2025-09-27 21:19:35 -0500

    feat: define tests parameters

--- PI/test_pi.sh
+++ PI/test_pi.sh
@@ -8,25 +8,27 @@ echo "Method,Implementation,Iterations,Units,Parameter1,Parameter2,Estimated_PI,
 
 # Compile programs
 gcc Needle/needle.c -o Needle/needle.out -lm
+gcc Needle/needle.c -o Needle/needle_O3.out -lm -O3
 gcc Needle/needle_threads.c -o Needle/needle_threads.out -lm -lpthread
 gcc Needle/needle_processes.c -o Needle/needle_processes.out -lm
 gcc Dartboard/dartboard.c -o Dartboard/dartboard.out -lm
+gcc Dartboard/dartboard.c -o Dartboard/dartboard_O3.out -lm -O3
 gcc Dartboard/dartboard_threads.c -o Dartboard/dartboard_threads.out -lm -lpthread
 gcc Dartboard/dartboard_processes.c -o Dartboard/dartboard_processes.out -lm
 
 # Test configurations
-ITERATIONS="1000 5000 10000"
-PARALLEL_UNITS="2 4 8"
+ITERATIONS="2000000 20000000 200000000 2000000000"
+PARALLEL_UNITS="2 4 8 16"
 
 # Needles: line_distance y needle_length
-LINE_DISTANCES="10 20"
-NEEDLE_LENGTHS="5 10"
+LINE_DISTANCES="1600 3200"
+NEEDLE_LENGTHS="400 800"
 
 # Dartboard: circle radius
-RADII="1 5 10"
+RADII="400 800 1600"
 
 # Number of runs
-NUM_RUNS=5
+NUM_RUNS=10
 
 ##################################
 #   BUFFON'S NEEDLE
@@ -43,6 +45,16 @@ for N in $ITERATIONS; do
         ERROR=$(echo "$RESULT" | grep "Error" | awk '{print $2}' | tr -d '%')
         TIME=$(echo "$RESULT" | grep "Time" | awk '{print $2}')
         echo "Needle,Secuencial,$N,1,$L,$LEN,$EST_PI,$DIFF,$ERROR,$TIME" >> $OUTPUT_FILE
+      done
+
+      # Sequential - O3
+      for ((run=1; run<=$NUM_RUNS; run++)); do
+        RESULT=$(./Needle/needle_O3.out $L $LEN $N | grep -E "Estimated|Difference|Error|Time")
+        EST_PI=$(echo "$RESULT" | grep "Estimated" | awk '{print $3}')
+        DIFF=$(echo "$RESULT" | grep "Difference" | awk '{print $2}')
+        ERROR=$(echo "$RESULT" | grep "Error" | awk '{print $2}' | tr -d '%')
+        TIME=$(echo "$RESULT" | grep "Time" | awk '{print $2}')
+        echo "Needle,Secuencial_O3,$N,1,$L,$LEN,$EST_PI,$DIFF,$ERROR,$TIME" >> $OUTPUT_FILE
       done
 
       # Threads
@@ -87,6 +99,16 @@ for N in $ITERATIONS; do
       ERROR=$(echo "$RESULT" | grep "Error" | awk '{print $2}' | tr -d '%')
       TIME=$(echo "$RESULT" | grep "Time" | awk '{print $2}')
       echo "Dartboard,Secuencial,$N,1,$R,-,$EST_PI,$DIFF,$ERROR,$TIME" >> $OUTPUT_FILE
+    done
+
+    # Sequential - O3
+    for ((run=1; run<=$NUM_RUNS; run++)); do
+      RESULT=$(./Dartboard/dartboard_O3.out $R $N | grep -E "Estimated|Difference|Error|Time")
+      EST_PI=$(echo "$RESULT" | grep "Estimated" | awk '{print $3}')
+      DIFF=$(echo "$RESULT" | grep "Difference" | awk '{print $2}')
+      ERROR=$(echo "$RESULT" | grep "Error" | awk '{print $2}' | tr -d '%')
+      TIME=$(echo "$RESULT" | grep "Time" | awk '{print $2}')
+      echo "Dartboard,Secuencial_O3,$N,1,$R,-,$EST_PI,$DIFF,$ERROR,$TIME" >> $OUTPUT_FILE
     done
 
     # Threads
